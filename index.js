aa230aa7df5118bdf042742113b69ce083a51812
Bohdan <52108700+Okashichan@users.noreply.github.com>
2022-10-30 00:33:35 +0300

    what

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ bot.onText(urlRe, async (msg, match) => {
             console.log(err.response.body);
         });
     } else if (dl?.imgs){
-        let interval = chatType === 'private' ? 3000 : dl.imgs.flat(1).length >= 18 ? 65000 : 3000;
+        let interval = chatType === 'private' ? 3000 : dl.imgs.length > 2 ? 65000 : 5000;
         dl.imgs.forEach(function (el, index) {
             setTimeout(function () {
                 console.log(`       part #${index+1}; size=${el.length}; timeout=${interval * index}`);
